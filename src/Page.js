91213d5c118392f64ed21ba110f618414eac3c94
daobeng <daniel.obeng@cbsinteractive.com>
2019-03-01 23:38:11 -0500

    sets up file structure

---
+++ src/Page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Intro from './Sections/Intro'
+import About from './Sections/About'
+import Contact from './Sections/Contact'
+import SampleWork from './Sections/SampleWork'
+import Footer from './Sections/Footer'
+
+export default class Page extends Component
+{
+
+}
